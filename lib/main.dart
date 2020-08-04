ccc5f904c13456e8bd41dce594ba83708b2f7c05
bablushaw23 <bablushaw23@gmail.com>
2020-08-04 11:26:18 +0530

    stateless widget in app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,5 @@ import 'src/app.dart';
 
 //2. define main function
 void main() {
-  //3. Create a new text widget to show on screen
-  var app =
-      //4. Take that widget and get on screen
-
-      runApp(App());
+  runApp(App());
 }
